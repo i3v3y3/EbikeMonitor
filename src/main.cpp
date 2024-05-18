ce5aee78b5af18ecc3b9523db0ce42ab37b6f9b7
John Kariuki Kinyanjui <74472544+johnkk1999@users.noreply.github.com>
2024-05-18 23:33:47 +0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -54,46 +54,44 @@ delay(10);
 }
 
 
-void speedometer(){
+// void speedometer(){
 
-//adding the sprite functions for the speedometer
-const float ANGLE_INCREMENT = pie / 5; // 32 degrees
-const float ANGLE_INCREMENT2 = pie / 25; // 12.8 degrees
-const int LINE_WIDTH = 3;
-const int LINE_WIDTH2 = 1;
+// //adding the sprite functions for the speedometer
+// const float ANGLE_INCREMENT = pie / 5; // 32 degrees
+// const float ANGLE_INCREMENT2 = pie / 25; // 12.8 degrees
+// const int LINE_WIDTH = 3;
+// const int LINE_WIDTH2 = 1;
 
 
-  // Draw more wide lines
- float angle2 = 0;
-  for (int i = 0; i < 27; i++) {
-    int16_t x1 = CENTER_X + (RADIUS-2) * cos(angle2);
-    int16_t y1 = CENTER_Y - (RADIUS-2) * sin(angle2);
-    int16_t x2 = CENTER_X + (RADIUS2) * cos(angle2);
-    int16_t y2 = CENTER_Y - (RADIUS2) * sin(angle2);
+//   // Draw more wide lines
+//  float angle2 = 0;
+//   for (int i = 0; i < 27; i++) {
+//     int16_t x1 = CENTER_X + (RADIUS-2) * cos(angle2);
+//     int16_t y1 = CENTER_Y - (RADIUS-2) * sin(angle2);
+//     int16_t x2 = CENTER_X + (RADIUS2) * cos(angle2);
+//     int16_t y2 = CENTER_Y - (RADIUS2) * sin(angle2);
 
-    tft.drawWideLine(x1, y1, x2, y2, LINE_WIDTH2, TFT_WHITE, TFT_BLACK);
-    angle2 += ANGLE_INCREMENT2;
-  }
-//Draw wide lines
-  float angle = 0;
-  for (int i = 0; i < 10; i++) {
-    //drawshort lines
-    int16_t x1 = CENTER_X + (RADIUS-2) * cos(angle);
-    int16_t y1 = CENTER_Y - (RADIUS-2) * sin(angle);
-    int16_t x2 = CENTER_X + (RADIUS2) * cos(angle);
-    int16_t y2 = CENTER_Y - (RADIUS2) * sin(angle);
-    tft.drawWideLine(x1, y1, x2, y2,LINE_WIDTH, TFT_RED, TFT_BLACK);
+//     tft.drawWideLine(x1, y1, x2, y2, LINE_WIDTH2, TFT_WHITE, TFT_BLACK);
+//     angle2 += ANGLE_INCREMENT2;
+//   }
+// //Draw wide lines
+//   float angle = 0;
+//   for (int i = 0; i < 10; i++) {
+//     //drawshort lines
+//     int16_t x1 = CENTER_X + (RADIUS-2) * cos(angle);
+//     int16_t y1 = CENTER_Y - (RADIUS-2) * sin(angle);
+//     int16_t x2 = CENTER_X + (RADIUS2) * cos(angle);
+//     int16_t y2 = CENTER_Y - (RADIUS2) * sin(angle);
+//     tft.drawWideLine(x1, y1, x2, y2,LINE_WIDTH, TFT_RED, TFT_BLACK);
     
-    angle += ANGLE_INCREMENT;
-    }
-  // Draw a smooth circle
-  //  tft.drawSmoothCircle(CENTER_X, CENTER_Y, RADIUS, TFT_MAROON, TFT_BLACK);
-  tft.drawSmoothArc(CENTER_X,CENTER_Y,RADIUS,RADIUS-2,90,270,TFT_MAROON,TFT_BLACK,true);
+//     angle += ANGLE_INCREMENT;
+//     }
+//   // Draw a smooth circle
+//   //  tft.drawSmoothCircle(CENTER_X, CENTER_Y, RADIUS, TFT_MAROON, TFT_BLACK);
+//   tft.drawSmoothArc(CENTER_X,CENTER_Y,RADIUS,RADIUS-2,90,270,TFT_MAROON,TFT_BLACK,true);
   
-  
+// }
 
-
-}
 void speedometerSprite(float pointerAngle)
 {   
   
@@ -146,30 +144,19 @@ const int LINE_WIDTH2 = 1;
 
 }
 
-void drawPointer(float pointerAngle)
-{
-  // float pointerAngle = 4*pie / 5;
-  int16_t x1 = CENTER_X + 4 * cos(pointerAngle-pie/2);
-  int16_t y1 = CENTER_Y - 4 * sin(pointerAngle-pie/2);
-  int16_t x2 = CENTER_X + 4 * cos(pointerAngle+pie/2);
-  int16_t y2 = CENTER_Y - 4 * sin(pointerAngle+pie/2);
-  int16_t x3 = CENTER_X + (RADIUS2-10) * cos(pointerAngle);
-  int16_t y3 = CENTER_Y - (RADIUS2-10) * sin(pointerAngle);
-   tft.fillCircle(CENTER_X,CENTER_Y,RADIUS2 - 8,TFT_BLACK);
-   tft.fillTriangle(x1,y1,x2,y2,x3,y3,TFT_GREEN);
-}
-// void drawPointerPrev(float pointerAngle)
+// void drawPointer(float pointerAngle)
 // {
 //   // float pointerAngle = 4*pie / 5;
-//   int16_t x1 = CENTER_X + 8 * cos(pointerAngle-pie/2);
-//   int16_t y1 = CENTER_Y - 8 * sin(pointerAngle-pie/2);
-//   int16_t x2 = CENTER_X + 8 * cos(pointerAngle+pie/2);
-//   int16_t y2 = CENTER_Y - 8 * sin(pointerAngle+pie/2);
+//   int16_t x1 = CENTER_X + 4 * cos(pointerAngle-pie/2);
+//   int16_t y1 = CENTER_Y - 4 * sin(pointerAngle-pie/2);
+//   int16_t x2 = CENTER_X + 4 * cos(pointerAngle+pie/2);
+//   int16_t y2 = CENTER_Y - 4 * sin(pointerAngle+pie/2);
 //   int16_t x3 = CENTER_X + (RADIUS2-10) * cos(pointerAngle);
 //   int16_t y3 = CENTER_Y - (RADIUS2-10) * sin(pointerAngle);
-//   //  tft.fillCircle(CENTER_X,CENTER_Y,RADIUS2 - 8,TFT_BLACK);
-//    tft.fillTriangle(x1,y1,x2,y2,x3,y3,TFT_BLACK);
+//    tft.fillCircle(CENTER_X,CENTER_Y,RADIUS2 - 8,TFT_BLACK);
+//    tft.fillTriangle(x1,y1,x2,y2,x3,y3,TFT_GREEN);
 // }
+
 
 void batteryManagementSystem()
 {
